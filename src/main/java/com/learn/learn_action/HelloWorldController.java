5dd2c89a6125162cda522e9006750da3130a397c
Phitchaya Charansathianchai <phitchaya.charansathianchai@ibm.com>
2024-07-16 14:56:03 +0700

    Init

---
+++ src/main/java/com/learn/learn_action/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.learn.learn_action;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld(@RequestParam(name = "name", required = false) String name) {
+        return (name == null) ? "Hello World" : "Hello " + name;
+    }
+
+}
---
+++ src/test/java/com/learn/learn_action/LearnActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.learn_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
