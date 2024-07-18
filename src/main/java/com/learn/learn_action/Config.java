7150bb7342fed39b91890f8f94012107b20f8dcc
Phitchaya Charansathianchai <phitchaya.charansathianchai@ibm.com>
2024-07-18 09:08:45 +0700

    Add config

---
+++ src/main/java/com/learn/learn_action/Config.java
@@ -0,0 +1,39 @@
+package com.learn.learn_action;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
+public class Config {
+
+    private String name;
+    private String environment;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+}
--- src/main/java/com/learn/learn_action/HelloWorldController.java
+++ src/main/java/com/learn/learn_action/HelloWorldController.java
@@ -1,15 +1,24 @@
 package com.learn.learn_action;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private Config config;
+
     @GetMapping("/")
     public String helloWorld(@RequestParam(name = "name", required = false) String name) {
         return (name == null) ? "Hello World 123" : "Hello " + name;
+    }
+
+    @GetMapping("/env-check")
+    public String getMethodName() {
+        return "This is " + config.getEnvironment() + " environment.";
     }
 
 }
